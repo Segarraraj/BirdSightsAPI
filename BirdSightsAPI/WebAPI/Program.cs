83657d4eefd13b615d04e4c4825fdc03fe0c3517
george <jsegarra@pixelratio.es>
2025-11-12 20:25:05 +0100

    Dependency injection

--- BirdSightsAPI/WebAPI/Program.cs
+++ BirdSightsAPI/WebAPI/Program.cs
@@ -1,15 +1,39 @@
+using Application.Interfaces;
+using Application.Services;
+using Domain.Entities;
+using Domain.Ports.Secondary;
+using MapperComponent.Mappers;
 using Microsoft.EntityFrameworkCore;
 using RepositoryComponent;
+using RepositoryComponent.Models;
+using RepositoryComponent.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add controllers
 builder.Services.AddControllers();
 
+// Add context
 builder.Services.AddDbContext<BirdSightsDBContext>(context =>
 {
     context.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerDev"));
 });
 
+// Add mappers
+builder.Services.AddScoped<IMapper<Bird, BirdModel>, BirdModelMapper>();
+builder.Services.AddScoped<IMapper<BirdModel, Bird>, BirdMapper>();
+builder.Services.AddScoped<IMapper<BirdSight, BirdSightModel>, BirdSightModelMapper>();
+builder.Services.AddScoped<IMapper<BirdSightModel, BirdSight>, BirdSightMapper>();
+
+// Add services
+builder.Services.AddScoped<IService<Bird>, BirdService>();
+builder.Services.AddScoped<IService<BirdSight>, BirdSightService>();
+
+// Add repositories
+builder.Services.AddScoped<IRepository<Bird>, BirdRepository>();
+builder.Services.AddScoped<IRepository<BirdSight>, BirdSightRepository>();
+
+// Add swagger
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
